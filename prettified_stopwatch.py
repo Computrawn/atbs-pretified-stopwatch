45397410eaed287c183c460b29daf308a7675a63
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-02 13:56:19 -0500

    Finish paste functionality. Add time_log.txt to ignore file.

--- prettified_stopwatch.py
+++ prettified_stopwatch.py
@@ -4,6 +4,7 @@
 
 import logging
 import time
+import pyperclip
 
 
 logging.basicConfig(
@@ -11,7 +12,7 @@ logging.basicConfig(
     filename="logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
-logging.disable(logging.CRITICAL)  # Note out to enable logging.
+# logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
 print(
@@ -24,6 +25,7 @@ print("Started.")
 start_time = time.time()
 last_time = start_time
 lap_num = 1
+output_file = []
 
 try:
     while True:
@@ -32,11 +34,19 @@ try:
         lap_time = str(round(time.time() - last_time, 2))
         lap_parenthesis = f"({lap_time})"
         total_time = str(round(time.time() - start_time, 2))
-        print(
-            f"Lap # {lap_num_str.ljust(4)} {total_time.rjust(12)} {lap_parenthesis.rjust(12)}",
-            end="",
-        )
+        output_string = f"Lap #{lap_num_str.ljust(4)}{total_time.rjust(12)}{lap_parenthesis.rjust(15)}"
+        print(output_string, end="")
+        output_file.append(output_string)
         lap_num += 1
         last_time = time.time()
 except KeyboardInterrupt:
     print("\nDone.")
+
+
+with open("time_log.txt", "w", encoding="utf-8") as wf:
+    for item in output_file:
+        wf.write(item + "\n")
+
+with open("time_log.txt", "r", encoding="utf-8") as rf:
+    compiled_output = rf.read()
+    pyperclip.copy(compiled_output)
